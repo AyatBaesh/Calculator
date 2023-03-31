7a84a54ecab84efa48daf3bc535ef31e7bf90992
buyshow <ayatbaeshev@gmail.com>
2023-03-31 14:31:23 +0200

    added basic functions and eventlisteners

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,143 @@
 const numButtons = document.querySelectorAll('.btn');
-const controlButtons = document.querySelectorAll('.control');
-const userInput = document.querySelector('#userInput');
+const operationButtons = document.querySelectorAll('.control');
+const currentInput = document.querySelector('#currentInput');
 const previousInput = document.querySelector('#previousInput');
-let temporary = 0;
-numButtons.forEach((button) => {
-    button.addEventListener('click',(event) => {
-        event.preventDefault();
-        userInput.textContent += button.value;
-        // let current = parseInt(userInput.textContent);
-    });
-
-});
-controlButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        previousInput.textContent += userInput.textContent;
-        temporary = parseInt(userInput.textContent);
-        console.log(`${button.value} was clicked`);
-        // console.log(`temporary ${temporary}, input ${input}`);
-        userInput.textContent ='';
-    });
+const clearButton = document.querySelector('#clear');
+let current = null;
+let previous = null;
+console.log(!current);
+numButtons.forEach((button) =>{
+    button.addEventListener('click', () => {
+        currentInput.textContent += button.value;   
+        
+    })
 });
 
+operationButtons.forEach((operationButton) => {
+    operationButton.addEventListener('click', () => {
+        current = parseInt(currentInput.textContent);
+        console.log(operationButton.textContent)
+        previousInput.textContent = currentInput.textContent + ' ' +  operationButton.textContent;
+        currentInput.textContent = ' ';
+            switch (operationButton.value){
+                case '+':   
+                    previousInput.textContent = add(previous, current);
+                    break;
+                case '-':   
+                    previousInput.textContent = subtract(previous, current);
+                    break;
+                case '/':   
+                    previousInput.textContent = multiply(previous, current);
+                    break;
+                case '*':   
+                    previousInput.textContent = divide(previous, current);
+                    break;
+                case '=': 
+                    console.log('= was clicked');
+            }
+        
+    })
+})
 
-// console.log(`${input}`)
+
+
+clearButton.addEventListener('click',() => {
+    currentInput.textContent = '';
+    previousInput.textContent = '';
+})
+
+
+
+
 function add(input1, input2){
+    console.log(`current:${input1}, previous:${input2}`);
+    console.log(input1 + input2);   
     return input1 + input2;
 }
+function subtract(input1, input2){
+    return input1 - input2;
+}
+function multiply(input1, input2){
+    return input1 * input2;
+}
+function divide(input1, input2){
+    return input1/input2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// numButtons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         previousInput.textContent += button.value;
+//         // let current = parseInt(previousInput.textContent);
+//     });
+
+// });
+// operationButtons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         console.log(previousInput.textContent)
+//         let current = parseInt(previousInput.textContent);
+//         previousInput.textContent = previousInput.textContent + button.value;
+//         previousInput.textContent = '';
+//         operationButtons.forEach((operationButton) => {
+//             let current = parseInt(previousInput.textContent);
+//             operationButton.addEventListener('click', () => {
+
+//             })
+//         })
+
+//     });
+// });
+
+
+// // console.log(`${input}`)
+
+
