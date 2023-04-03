31e9736aa9b3b80c1dfbee09e003163471d53908
buyshow <ayatbaeshev@gmail.com>
2023-04-03 19:12:26 +0200

    work in progress

--- scripts/index.js
+++ scripts/index.js
@@ -3,69 +3,81 @@ const operationButtons = document.querySelectorAll('.control');
 const currentInput = document.querySelector('#currentInput');
 const previousInput = document.querySelector('#previousInput');
 const clearButton = document.querySelector('#clear');
+const keyboard = document.querySelector('.keyboard');
 let current = null;
 let previous = null;
 let result = null;
-console.log(!current);
-numButtons.forEach((button) =>{
-    button.addEventListener('click', () => {
-        currentInput.textContent += button.value;   
+let operation = '';
+
+keyboard.addEventListener('click', (event) => {
+    if(event.target.classList.contains('btn')){
+        if(parseInt(currentInput.textContent) === result){
+            previousInput.textContent = currentInput.textContent + operation;
+            currentInput.textContent = '';
+            currentInput.textContent += event.target.value;
+        }else{
+            currentInput.textContent += event.target.value;
+        }
+    }else if(event.target.classList.contains('control')){
         
-    })
-});
-
-operationButtons.forEach((operationButton) => {
-    operationButton.addEventListener('click', () => {
         current = parseInt(currentInput.textContent);
-        console.log(operationButton.textContent);
-
         if(!previous){
             previous = current;
-            previousInput.textContent = currentInput.textContent +  operationButton.textContent;
-            currentInput.textContent = ' ';
+            previousInput.textContent = currentInput.textContent +  event.target.textContent;
+            currentInput.textContent = '';
             return;
         }
-        switch (operationButton.value){
-            case '+':   
-                result = add(previous, current);                   
-                break;
-            case '-':   
-                result = subtract(previous, current);
-                break;
-            case '/':                  
-                result = divide(previous, current);
-                break;
-            case '*':   
-                result = multiply(previous, current);
-                break;
-            case '=': 
-            console.log(!result);
-                if(!result){
-                    return;
-                }
+
+        if(!operation){
+            operation = event.target.value;
+            result = getResult(operation);
+            console.log(`result: ${result}`)
+
+            
+        }else{
+            previous = result;
+            previousInput.textContent = result;
+            let newOperation = event.target.value;
+            previous = result;
         }
-
-        previousInput.textContent = `${previous} ${operationButton.value} ${current}` 
-        currentInput.textContent = result;
+        previousInput.textContent = `${previous} ${event.target.value} ${current} = ` 
+        currentInput.textContent = `${result}`;
         
-    })
+    }else if(event.target.textContent === "CLEAR"){
+        currentInput.textContent = '';
+        
+        previousInput.textContent = '';
+        current = null;
+        previous = null;
+    }
 })
 
 
-
-clearButton.addEventListener('click',() => {
-    currentInput.textContent = '';
-    previousInput.textContent = '';
-    current = null;
-    previous = null;
-})
-
-
-
+function getResult(operation){
+    switch (operation){
+        case '+':   
+            result = add(previous, current);                   
+            break;
+        case '-':   
+            result = subtract(previous, current);
+            break;
+        case '/':                  
+            result = divide(previous, current);
+            break;
+        case '*':   
+            result = multiply(previous, current);
+            break;
+        case '=': 
+        console.log(!result);
+            if(!result){
+                return result;
+            }
+    }
+    return result;
+}
 
 function add(input1, input2){
-    console.log(`current:${input2}, previous:${input1}`);
-    console.log(input1 + input2);   
+    console.log(`current:${input2}, previous:${input1}`);   
     return input1 + input2;
 }
 function subtract(input1, input2){
